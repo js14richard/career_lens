14194b623e52f4dcd886cac7c03be1bb9fdffa26
Siril Richard <js14richard@gmail.com>
2025-12-06 19:13:27 +0530

    DB config setup

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Home route test");
+});
+
+export default app;
